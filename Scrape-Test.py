a866758bec704c9f9f2098072cb237f2d2dc1920
lelajcorson <lelajcorson@gmail.com>
2022-08-15 14:22:56 -0700

    continued working on webscraping
    -created a function to get the stats table data as a pandas dataFrame
    -started working on a function to navigate to each player's page with selenium webdriver

--- Scrape-Test.py
+++ Scrape-Test.py
@@ -1,21 +1,66 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
 import pandas as pd
 import requests
 
+
+url = requests.get("http://stats.ncaa.org/player/index?id=15940&org_id=756&stats_player_seq=1973760&year_stat_category_id=14961", headers = {"User-Agent" : "Mozilla/5.0"})
+
 option = webdriver.ChromeOptions()
 option.add_argument('--headless')
+driver = webdriver.Chrome(service = Service("C:\Lela\Coding\chromedriver_win32\chromedriver"), options = option)
 
-driver = webdriver.Chrome(service = Service("C:/Users/lelaj/Downloads/chromedriver_win32/chromedriver"), options = option)
+def getPlayerURL(name):
+    driver.get("https://www.google.com/")
+    driver.maximize_window()
+    print(name)
+    search = driver.find_element(By.XPATH, "//input[@type = 'text']")
+    search.send_keys(name)
 
-driver.get('http://stats.ncaa.org/player/index?id=15940&org_id=756&stats_player_seq=1973760&year_stat_category_id=14961') 
+getPlayerURL("gabbie plain")
+def getStats(url):
+    tables = pd.read_html(url.text, match = "Schedule/Results")
+    stats_table_temp = tables[1]
+    stats_df = pd.DataFrame(stats_table_temp.values[2:], columns = stats_table_temp.iloc[1])
+    print(stats_df)
 
-soup = BeautifulSoup(driver.page_source, 'html.parser') 
+# html_content = requests.get(url, headers = {'User-Agent': 'Chrome'}).text
+
+# soup = BeautifulSoup(html_content, "lxml")
+# #print(soup.prettify())
+
+# gdp = soup.find_all("table")
+# print(gdp)
+
+
+# def getTable(url):
+#     html_content = requests.get(url, headers = {'User-Agent': 'Chrome'}).text
+#     soup = BeautifulSoup(html_content, "lxml")
+
+#     big_div = soup.find(id = "game_breakdown_div") #the div containing the stats table
+#     table = big_div.find("table", class_= "mytable") #the stats table
+#     row_list = table.find_all("tr")
+#     row_list.pop(0) #removing the title and links
+    
+    
+
+#getTable(url)
+
+# option = webdriver.ChromeOptions()
+# option.add_argument('--headless')
+
+# driver = webdriver.Chrome(service = Service("C:/Users/lelaj/Downloads/chromedriver_win32/chromedriver"), options = option)
+
+# driver.get('http://stats.ncaa.org/player/index?id=15940&org_id=756&stats_player_seq=1973760&year_stat_category_id=14961') 
+
+# soup = BeautifulSoup(driver.page_source, 'html.parser') 
  
-table = soup.select("#game_breakdown_div")
-ips = soup.select("#game_breakdown_div tr td")
+# table = soup.select("#game_breakdown_div")
+# ips = soup.select("#game_breakdown_div tr td")
 
-huh = pd.read_html(requests.get("http://stats.ncaa.org/player/index?id=15940&org_id=756&stats_player_seq=1973760&year_stat_category_id=14961", headers = {'User-agent': 'Mozilla/5.0'}).text)
+# huh = pd.read_html(requests.get("http://stats.ncaa.org/player/index?id=15940&org_id=756&stats_player_seq=1973760&year_stat_category_id=14961", headers = {'User-agent': 'Mozilla/5.0'}).text)
 
-print(huh)
+# print(huh)
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from asyncio.windows_events import NULL
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session, flash
+from functools import wraps
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.service import Service
@@ -9,10 +11,32 @@ import requests
 app = Flask(__name__)
 
 #setting up web scraping
-option = webdriver.ChromeOptions()
-option.add_argument('--headless')
+# option = webdriver.ChromeOptions()
+# option.add_argument('--headless')
 
-driver = webdriver.Chrome(service = Service("C:/Users/lelaj/Downloads/chromedriver_win32/chromedriver"), options = option)
+# driver = webdriver.Chrome(service = Service("C:/Users/lelaj/Downloads/chromedriver_win32/chromedriver"), options = option)
+
+#web scraping
+def getTable(url):
+    html_content = requests.get(url, headers = {'User-Agent': 'Chrome'}).text
+    soup = BeautifulSoup(html_content, "lxml")
+
+    table_list = "Schedule/Results".find_parent("div", class = "game_breakdown_div")
+    print(table_list)
+
+
+currentProfile = NULL
+app.secret_key = "hi"
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 #setting up profiles
 profiles = [{
@@ -31,6 +55,11 @@ profiles = [{
 def home():
     return render_template('index.html')
 
+@app.route("/welcome")
+@login_required
+def welcome():
+    return render_template('welcome.html')
+
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     error = None
@@ -39,11 +68,20 @@ def login():
         for user in profiles:
             print(user['username'] + " " + user['password'])
             if request.form['username'] == user['username']and request.form['password'] == user['password']:
-                return redirect(url_for('home'))
-                error = none
+                error = None
+                session['logged_in'] = True
+                flash('You were just logged in!')
+                return redirect(url_for('welcome'))
             else:
                 error = 'Incorrect username or password'
 
     return render_template('login.html', error = error)
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were just logged out!')
+    return redirect(url_for('home'))
 
 app.run(debug = True)
